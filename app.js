cbbb147e409695ace7cbd58e3f83da79908d8abc
yuwantha98 <yuwantharavihara123@gmail.com>
2024-06-19 14:05:02 +0530

    create the same function in four ways

--- app.js
+++ app.js
@@ -1,91 +1,113 @@
-console.log((document.getElementById("heading").innerHTML = "Saman"));
+// console.log((document.getElementById("heading").innerHTML = "Saman"));
 
-console.log(document.getElementById("username"));
-console.log(document.getElementById("password"));
-console.log(document.getElementById("age"));
-console.log(document.getElementById("btn_login_onAction"));
+// console.log(document.getElementById("username"));
+// console.log(document.getElementById("password"));
+// console.log(document.getElementById("age"));
+// console.log(document.getElementById("btn_login_onAction"));
 
-document
-  .getElementById("btn_login_onAction")
-  .addEventListener("click", loginOnAction);
+// document
+//   .getElementById("btn_login_onAction")
+//   .addEventListener("click", loginOnAction);
 
-function loginOnAction() {
-  let userName = document.getElementById("username").value;
-  let userPassword = document.getElementById("password").value;
-  let userAge = document.getElementById("age").value;
+// function loginOnAction() {
+//   let userName = document.getElementById("username").value;
+//   let userPassword = document.getElementById("password").value;
+//   let userAge = document.getElementById("age").value;
 
-  if (userAge > 18) {
-    console.log("you are eligible!");
-    alert("you are eligible!");
-  } else {
-    console.log("you are not eligible!");
-    alert("you are not eligible!");
-  }
+//   if (userAge > 18) {
+//     console.log("you are eligible!");
+//     alert("you are eligible!");
+//   } else {
+//     console.log("you are not eligible!");
+//     alert("you are not eligible!");
+//   }
 
-  alert("Hello");
+//   alert("Hello");
 
-  console.log(userName);
-  console.log(userPassword);
-  console.log(userAge);
+//   console.log(userName);
+//   console.log(userPassword);
+//   console.log(userAge);
 
-  console.log(typeof userName);
-  console.log(typeof userPassword);
-  console.log(typeof userAge);
+//   console.log(typeof userName);
+//   console.log(typeof userPassword);
+//   console.log(typeof userAge);
+// }
+
+// let numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// numbers.push(100);
+
+// console.log(numbers);
+
+// const customers = [
+//   {
+//     name: "saman",
+//     age: 15,
+//     address: "gampaha",
+//   },
+//   {
+//     name: "kamal",
+//     age: 10,
+//     address: "kalutar",
+//   },
+//   {
+//     name: "nimal",
+//     age: 16,
+//     address: "galle",
+//   },
+//   {
+//     name: "sunil",
+//     age: 22,
+//     address: "matara",
+//   },
+// ];
+
+// console.log(customers[0].name);
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let isSubscribed = false;
+//   let subscriberCount = 0;
+//   const subscribeButton = document.getElementById("subscribe");
+//   const subscribersDisplay = document.getElementById("subscribers");
+
+//   subscribeButton.addEventListener("click", function () {
+//     if (isSubscribed) {
+//       isSubscribed = false;
+//       subscribeButton.textContent = "Subscribe";
+//       subscribeButton.style.backgroundColor = "red";
+//       subscriberCount--;
+//     } else {
+//       isSubscribed = true;
+//       subscribeButton.textContent = "Unsubscribe";
+//       subscribeButton.style.backgroundColor = "gray";
+//       subscriberCount++;
+//     }
+//     subscribersDisplay.textContent = `Subscribers: ${subscriberCount}`;
+//   });
+// });
+
+const customer = {
+  name: "saman",
+  age: 12,
+  run: () => console.log("Hello.."),
+};
+
+customer.run();
+
+function sample() {
+  console.log("Hello..");
 }
 
-let numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+sample = function () {
+  console.log("Hello..");
+};
 
-for (let i = 0; i < numbers.length; i++) {
-  console.log(numbers[i]);
-}
+sample = () => {
+  console.log("Hello..");
+};
 
-numbers.push(100);
-
-console.log(numbers);
-
-const customers = [
-  {
-    name: "saman",
-    age: 15,
-    address: "gampaha",
-  },
-  {
-    name: "kamal",
-    age: 10,
-    address: "kalutar",
-  },
-  {
-    name: "nimal",
-    age: 16,
-    address: "galle",
-  },
-  {
-    name: "sunil",
-    age: 22,
-    address: "matara",
-  },
-];
-
-console.log(customers[0].name);
-
-document.addEventListener("DOMContentLoaded", function () {
-  let isSubscribed = false;
-  let subscriberCount = 0;
-  const subscribeButton = document.getElementById("subscribe");
-  const subscribersDisplay = document.getElementById("subscribers");
-
-  subscribeButton.addEventListener("click", function () {
-    if (isSubscribed) {
-      isSubscribed = false;
-      subscribeButton.textContent = "Subscribe";
-      subscribeButton.style.backgroundColor = "red";
-      subscriberCount--;
-    } else {
-      isSubscribed = true;
-      subscribeButton.textContent = "Unsubscribe";
-      subscribeButton.style.backgroundColor = "gray";
-      subscriberCount++;
-    }
-    subscribersDisplay.textContent = `Subscribers: ${subscriberCount}`;
-  });
-});
+sample = () => console.log("Hello..");
